89ec247f8abec111dc3b3028ae4f37ce8ce145e2
roberto-m-a <18161174@itoaxaca.edu.mx>
2024-08-11 23:59:04 -0600

    d

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
-{
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
+    public function boot() {
+        
     }
-}
 }
